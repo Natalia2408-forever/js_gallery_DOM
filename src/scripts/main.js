80a4b643630a35908940466726240c73fac934b3
Natalia Panchuk <Foreverdanda@ukr.net>
2026-02-07 14:01:15 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,27 @@
 'use strict';
+
+const gallery = document.querySelector('.gallery');
+
+gallery.addEventDateListener('click', (eventdate) => {
+  const target = eventdate.target;
+  let link;
+  let url;
+
+  if (target.tagName === 'IMG') {
+    link = target.closest('a') || target.parentElement;
+  } else if (target.tagName === 'A') {
+    link = target;
+  }
+
+  if (link) {
+    url = link.getAttribute('href') || link.dataset.src;
+  }
+
+  if (link.tagName === 'A') {
+    eventdate.preventdateDefault();
+  }
+
+  const bigImageEl = document.querySelector('.large-img');
+
+  bigImageEl.src = url;
+});
